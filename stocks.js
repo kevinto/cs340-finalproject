c3471f56f69955a215e7967f5683d015ba280afd
U-OMNICELL\kevint <kevint@OMCL764-0HHLFAH.omnicell.com>
2015-03-05 08:13:47 -0800

    implemented add a stock, add a bank acct, add stock order.

--- stocks.js
+++ stocks.js
@@ -1,33 +1,195 @@
-function findCustInfo() {
+// Inserts a new stock order into the database
+function insertNewStockOrder() {
+  // Get form values
+  var ordStatus = document.getElementsByName('orderStatusSelectInsert');
+  ordStatus = ordStatus[0].options[ordStatus[0].selectedIndex].value; 
 
-  // Define Async return function
-  var viewOneCustFunc = function(request){
+  var ordType = document.getElementsByName('orderTypeSelectInsert');
+  ordType = ordType[0].options[ordType[0].selectedIndex].value; 
+
+  var qtyOrdered = document.getElementById('qtyOrdInsert').value;
+  var ordStartDate = document.getElementById('ordStartDateInsert').value;
+
+  var stkSym = document.getElementsByName('ordStockSelectInsert');
+  stkSym = stkSym[0].options[stkSym[0].selectedIndex].value;
+
+  var custSsId = document.getElementsByName('ordCustSelectInsert');
+  custSsId = custSsId[0].options[custSsId[0].selectedIndex].value;
+
+  // Create Return function
+  var insertStockOrderFunc = function(request){
     return function() {
       if(request.readyState == 4) {
-        var containerId = 'custView';
-        var container = document.getElementById(containerId);
+        if (request.responseText === 'EmptyParams') {
+          alert('One of the values you entered for a new stock order insertion was empty. Please fill out the required fields and resubmit.');
+        }
+        else {
+          alert('Stock Order Add Successful');
+        }
 
-        var resultObj = JSON.parse(request.responseText);
-        // container.innerText = resultObj.first_name + ' ' + resultObj.last_name + ' ' + resultObj.social_security_num;
-        var tableParamObj = new Array();
-        tableParamObj.push(resultObj);
-        addTable(containerId, tableParamObj);
+        location.reload();
       }
     }
   };
 
-  // Create object that holds the SQL query
-  var selectedOption = document.getElementsByName('viewCustSelect');
-  selectedOption = selectedOption[0];
-  var custParams = {
-    ssNum: selectedOption.options[selectedOption.selectedIndex].value
+  // Create Php parameters
+  var stkOrderParams = {
+    ordStatus: ordStatus,
+    ordType: ordType,
+    qtyOrdered: qtyOrdered,
+    ordStartDate: ordStartDate,
+    stkSym: stkSym,
+    custSsId: custSsId 
   };
 
-  callStockPhp('getOneCustInfo', viewOneCustFunc, custParams);
+  callStockPhp('insertNewStockOrder', insertStockOrderFunc, stkOrderParams);
 
   return false;
 }
 
+// Inserts a new customer bank account into the database
+function insertNewBankAcct() {
+  // Get form values
+  var bankName = document.getElementById('bankNameInsert').value;
+  var bankAcctNum = document.getElementById('bankAcctNumInsert').value;
+  var custSsId = document.getElementsByName('addCustBankSelect');
+  custSsId = custSsId[0].options[custSsId[0].selectedIndex].value;
+
+  // Create Return function
+  var insertNewBankAcctFunc = function(request){
+    return function() {
+      if(request.readyState == 4) {
+        if (request.responseText === 'EmptyParams') {
+          alert('One of the values you entered for a new bank account insertion was empty. Please fill out the required fields and resubmit.');
+        }
+        else {
+          alert('Bank Account Add Successful');
+        }
+
+        location.reload();
+      }
+    }
+  };
+
+  // Create Php parameters
+  var bankParams = {
+    bankName: bankName,
+    bankAcctNum: bankAcctNum,
+    custSsId: custSsId 
+  };
+
+  callStockPhp('insertNewBankAcct', insertNewBankAcctFunc, bankParams);
+
+  return false;
+}
+
+// Inserts a new fee into the database
+function insertNewFee() {
+  // Get form values
+  var feeName = document.getElementById('feeNameInsert').value;
+  var feeAmt = document.getElementById('feeAmtInsert').value;
+  var custSsId = document.getElementsByName('addFeeCustSelect');
+  custSsId = custSsId[0].options[custSsId[0].selectedIndex].value;
+
+  // Create Return function
+  var insertNewFeeFunc = function(request){
+    return function() {
+      if(request.readyState == 4) {
+        if (request.responseText === 'EmptyParams') {
+          alert('One of the values you entered for a new fee insertion was empty. Please fill out the required fields and resubmit.');
+        }
+        else {
+          alert('Fee Add Successful');
+        }
+
+        location.reload();
+      }
+    }
+  };
+
+  // Create Php parameters
+  var feeParams = {
+    feeName: feeName,
+    feeAmt: feeAmt,
+    custSsId: custSsId 
+  };
+
+  callStockPhp('insertNewFee', insertNewFeeFunc, feeParams);
+
+  return false;
+}
+
+// Inserts a new stock into the database
+function insertNewStock() {
+  // Get form values
+  var stkName = document.getElementById('stockNameInsert').value;
+  var stkSym = document.getElementById('stockSymbolInsert').value;
+  var stkPrice = document.getElementById('stockPriceInsert').value;
+
+  // Create Return function
+  var insertNewStkFunc = function(request){
+    return function() {
+      if(request.readyState == 4) {
+        if (request.responseText === 'EmptyParams') {
+          alert('One of the values you entered for a new stock insertion was empty. Please fill out the required fields and resubmit.');
+        }
+        else {
+          alert('Stock Add Successful');
+        }
+
+        location.reload();
+      }
+    }
+  };
+
+  // Create Php parameters
+  var stockParams = {
+    stkName: stkName,
+    stkSym: stkSym,
+    stkPrice: stkPrice 
+  };
+
+  callStockPhp('insertNewStk', insertNewStkFunc, stockParams);
+
+  return false;
+}
+
+// Inserts a new customer into the database
+function insertNewCust() {
+  // Get form values
+  var firstName = document.getElementById('firstNameInsert').value;
+  var lastName = document.getElementById('lastNameInsert').value;
+  var soSecNum = document.getElementById('soSecNumInsert').value;
+
+  // Create Return function
+  var insertNewCustFunc = function(request){
+    return function() {
+      if(request.readyState == 4) {
+        if (request.responseText === 'EmptyParams') {
+          alert('One of the values you entered for a new customer insertion was empty. Please fill out the required fields and resubmit.');
+        }
+        else {
+          alert('Customer Add Successful');
+        }
+
+        location.reload();
+      }
+    }
+  };
+
+  // Create Php parameters
+  var custParams = {
+    firstName: firstName,
+    lastName: lastName,
+    soSecNum: soSecNum
+  };
+
+  callStockPhp('insertNewCust', insertNewCustFunc, custParams);
+
+  return false;
+}
+
+// GENERIC HELPER FUNCTIONS
 function addTable(targetDiv, dispObjArray) {
       
   var myTableDiv = document.getElementById(targetDiv);
@@ -37,17 +199,27 @@ function addTable(targetDiv, dispObjArray) {
   
   for (var i = 0; i < dispObjArray.length; i++){
     // Create the headers
-
-   // Create the actual rows 
-
     var tr = document.createElement('tr');
     table.appendChild(tr);
+    for (var property in dispObjArray[i]) {
+      if (dispObjArray[i].hasOwnProperty(property)) {
 
-    for (var j=0; j<4; j++){
-       var td = document.createElement('td');
-       td.width='75';
-       td.appendChild(document.createTextNode("Cell " + i + "," + j));
-       tr.appendChild(td);
+        var th = document.createElement('th');
+        th.appendChild(document.createTextNode(property));
+        tr.appendChild(th);
+      }
+    }
+
+    // Create the rows for the data
+    tr = document.createElement('tr');
+    table.appendChild(tr);
+    for (var property in dispObjArray[i]) {
+      if (dispObjArray[i].hasOwnProperty(property)) {
+
+        var td = document.createElement('td');
+        td.appendChild(document.createTextNode(dispObjArray[i][property]));
+        tr.appendChild(td);
+      }
     }
   }
 
@@ -72,7 +244,6 @@ function callStockPhp(phpFuncName, returnFunc, optionalParams) {
     }
   }
 
-
   if (!request){
     return false;
   }
@@ -85,27 +256,95 @@ function callStockPhp(phpFuncName, returnFunc, optionalParams) {
 
 function populateComboBoxes() {
   popAllCustomerComboBoxes();
+  popAllStockComboBoxes();
 }
 
-// Populate Combo Box in Customer view table section
-function popAllCustomerComboBoxes() {
+// VIEW DATA FUNCTIONS:
+
+// Populate all stock combo boxes 
+function popAllStockComboBoxes() {
   var popCustComBoFunc = function(request){
     return function() {
       if(request.readyState == 4) {
-        var container = document.getElementsByName('viewCustSelect');
-        container = container[0];
+        var stkContainers = new Array();
+        stkContainers.push(document.getElementsByName('ordStockSelectInsert'));
 
-        var objArray = JSON.parse(request.responseText);
-        var currentItem;
-        var fullName;
-        var i;
-        for(i = 0; i < objArray.length; i++) {
-          currentItem = JSON.parse(objArray[i]);
-          fullName = currentItem.first_name + ' ' + currentItem.last_name;
-          container.options[container.options.length] = new Option(fullName, currentItem.social_security_num);
+        for (var i = 0; i < stkContainers.length; i++) {
+          var objArray = JSON.parse(request.responseText);
+          var currentItem;
+          for(var j = 0; j < objArray.length; j++) {
+            currentItem = JSON.parse(objArray[j]);
+            stkSym = currentItem.stock_symbol;
+
+            // stkContainers is an array of arrays of the select element 
+            stkContainers[i][0].options[stkContainers[i][0].options.length] = new Option(stkSym, stkSym);
+          }
         }
       }
     }
   };
-  callStockPhp('getAllCustInfo',popCustComBoFunc);
+  callStockPhp('getAllStockSym', popCustComBoFunc);
+}
+
+// Populate all customer combo boxes
+function popAllCustomerComboBoxes() {
+  var popCustComBoFunc = function(request){
+    return function() {
+      if(request.readyState == 4) {
+        var custContainers = new Array();
+        custContainers.push(document.getElementsByName('viewCustSelect'));
+        custContainers.push(document.getElementsByName('addFeeCustSelect'));
+        custContainers.push(document.getElementsByName('addCustBankSelect'));
+        custContainers.push(document.getElementsByName('ordCustSelectInsert'));
+
+        for (var i = 0; i < custContainers.length; i++) {
+          var objArray = JSON.parse(request.responseText);
+          var currentItem;
+          var fullName;
+          for(var j = 0; j < objArray.length; j++) {
+            currentItem = JSON.parse(objArray[j]);
+            fullName = currentItem.first_name + ' ' + currentItem.last_name;
+
+            // custContainers is an array of arrays of the select element 
+            custContainers[i][0].options[custContainers[i][0].options.length] = new Option(fullName, currentItem.social_security_num);
+          }
+        }
+      }
+    }
+  };
+  callStockPhp('getAllCustInfo', popCustComBoFunc);
+}
+
+// Displays the individual customer information
+function findCustInfo() {
+
+  // Define Async return function
+  var viewOneCustFunc = function(request){
+    return function() {
+      if(request.readyState == 4) {
+        var containerId = 'custView';
+        var container = document.getElementById(containerId);
+
+        var resultObj = JSON.parse(request.responseText);
+
+        // Clear out empty message 
+        container.innerText = '';
+
+        var tableParamObj = new Array();
+        tableParamObj.push(resultObj);
+        addTable(containerId, tableParamObj);
+      }
+    }
+  };
+
+  // Create object that holds the SQL query
+  var selectedOption = document.getElementsByName('viewCustSelect');
+  selectedOption = selectedOption[0];
+  var custParams = {
+    ssNum: selectedOption.options[selectedOption.selectedIndex].value
+  };
+
+  callStockPhp('getOneCustInfo', viewOneCustFunc, custParams);
+
+  return false;
 }
--- stocks.php
+++ stocks.php
@@ -11,16 +11,264 @@
   }
 
   if ($_SERVER['REQUEST_METHOD'] === 'GET' && count($_GET) > 0) {
+
     if (isset($_GET['getAllCustInfo'])) {
       getAllCustomerRecords();
+      die();
     }
 
+    if (isset($_GET['getAllStockSym'])) {
+      getAllStockSymbols();
+      die();
+    }
+
+    // Get view of one customer
     if (isset($_GET['getOneCustInfo']) && isset($_GET['ssNum'])) {
-      getSingleCustomerInfo();
+      getSingleCustomerInfo($_GET['ssNum']);
+      die();
+    }
+
+    // Insert one new customer
+    if (isset($_GET['insertNewCust']) && isset($_GET['firstName']) 
+      && isset($_GET['lastName']) && isset($_GET['soSecNum'])) {
+
+      // Check if there are any empty parameters
+      if ($_GET['firstName'] !== '' && $_GET['lastName'] !== '' 
+        && $_GET['soSecNum'] !== '') {
+
+        insertNewCustomer($_GET['firstName'], $_GET['lastName'], $_GET['soSecNum']);
+        die();
+      }
+      else {
+        echo 'EmptyParams';
+        die();
+      }
+    }
+
+    // Insert one new stock 
+    if (isset($_GET['insertNewStk']) && isset($_GET['stkName']) 
+      && isset($_GET['stkSym']) && isset($_GET['stkPrice'])) {
+
+      // Check if there are any empty parameters
+      if ($_GET['stkName'] !== '' && $_GET['stkSym'] !== '' 
+        && $_GET['stkPrice'] !== '') {
+
+        insertNewStk($_GET['stkName'], $_GET['stkSym'], $_GET['stkPrice']);
+        die();
+      }
+      else {
+        echo 'EmptyParams';
+        die();
+      }
+    }
+
+    // Insert one new fee 
+    if (isset($_GET['insertNewFee']) && isset($_GET['feeName']) 
+      && isset($_GET['feeAmt']) && isset($_GET['custSsId'])) {
+
+      // Check if there are any empty parameters
+      if ($_GET['feeName'] !== '' && $_GET['feeAmt'] !== '' 
+        && $_GET['custSsId'] !== '') {
+
+        insertNewFee($_GET['feeName'], $_GET['feeAmt'], $_GET['custSsId']);
+        die();
+      }
+      else {
+        echo 'EmptyParams';
+        die();
+      }
+    }
+
+    // Insert one new bank account 
+    if (isset($_GET['insertNewBankAcct']) && isset($_GET['bankName']) 
+      && isset($_GET['bankAcctNum']) && isset($_GET['custSsId'])) {
+
+      // Check if there are any empty parameters
+      if ($_GET['bankName'] !== '' && $_GET['bankAcctNum'] !== '' 
+        && $_GET['custSsId'] !== '') {
+
+        insertNewBankAcct($_GET['bankName'], $_GET['bankAcctNum'], $_GET['custSsId']);
+        die();
+      }
+      else {
+        echo 'EmptyParams';
+        die();
+      }
+    }
+
+    // Insert one new stock order 
+    if (isset($_GET['insertNewStockOrder']) && isset($_GET['ordStatus']) 
+      && isset($_GET['ordType']) && isset($_GET['qtyOrdered'])
+      && isset($_GET['ordStartDate']) && isset($_GET['stkSym'])
+      && isset($_GET['custSsId'])) {
+
+      // Check if there are any empty parameters
+      if ($_GET['ordStatus'] !== '' && $_GET['ordType'] !== '' 
+        && $_GET['qtyOrdered'] !== '' && $_GET['ordStartDate'] !== '' 
+        && $_GET['stkSym'] !== '' && $_GET['custSsId'] !== '') {
+
+        insertNewStockOrder($_GET['ordStatus'], $_GET['ordType'], $_GET['qtyOrdered'], 
+          $_GET['ordStartDate'], $_GET['stkSym'], $_GET['custSsId']);
+
+        die();
+      }
+      else {
+        echo 'EmptyParams';
+        die();
+      }
     }
   }
 
-  function getSingleCustomerInfo() {
+  function insertNewStockOrder($ordStatus, $ordType, $qtyOrdered, $ordStartDate, $stkSym, $custSsId) {
+    global $mysqli;
+
+    $custId = getCustomerDbKey($custSsId);
+    $stkId = getStockDbKey($stkSym);
+
+    // Prepare the insert statment
+    if (!($stmt = $mysqli->prepare("INSERT INTO stock_orders(order_status, order_type, qty_ordered, order_start_date, cust_id, stock_id) VALUES (?, ?, ?, ?, ?, ?)"))) {
+      echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+      die();
+    }
+
+    // Add values to SQL insert statement
+    if (!$stmt->bind_param("ssisii", $ordStatus, $ordType, $qtyOrdered, $ordStartDate, $custId, $stkId)) {
+      echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
+      die();
+    }
+
+    // Execute sql statement
+    if (!$stmt->execute()) {
+      echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+      die();
+    }
+  }
+
+  function insertNewBankAcct($bankName, $bankAcctNum, $custSsId) {
+    global $mysqli;
+
+    $custId = getCustomerDbKey($custSsId);
+
+    // Prepare the insert statment
+    if (!($stmt = $mysqli->prepare("INSERT INTO bank_acct(bank_name, account_num, cust_id) VALUES (?, ?, ?)"))) {
+      echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+      die();
+    }
+
+    // Add values to SQL insert statement
+    if (!$stmt->bind_param("sii", $bankName, $bankAcctNum, $custId)) {
+      echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
+      die();
+    }
+
+    // Execute sql statement
+    if (!$stmt->execute()) {
+      echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+      die();
+    }
+  }
+
+  function insertNewFee($feeName, $feeAmt, $custSsId) {
+    global $mysqli;
+
+    $custId = getCustomerDbKey($custSsId);
+
+    // Prepare the insert statment
+    if (!($stmt = $mysqli->prepare("INSERT INTO fees(fee_name, fee_amount, cust_id) VALUES (?, ?, ?)"))) {
+      echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+      die();
+    }
+
+    // Add values to SQL insert statement
+    if (!$stmt->bind_param("sii", $feeName, $feeAmt, $custId)) {
+      echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
+      die();
+    }
+
+    // Execute sql statement
+    if (!$stmt->execute()) {
+      echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+      die();
+    }
+  }
+
+  function insertNewStk($stockName, $stockSymbol, $stockPrice) {
+    global $mysqli;
+
+    // Prepare the insert statment
+    if (!($stmt = $mysqli->prepare("INSERT INTO stocks(stock_name, stock_symbol, stock_price) VALUES (?, ?, ?)"))) {
+      echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+      die();
+    }
+
+    // Add values to SQL insert statement
+    if (!$stmt->bind_param("ssi", $stockName, $stockSymbol, $stockPrice)) {
+      echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
+      die();
+    }
+
+    // Execute sql statement
+    if (!$stmt->execute()) {
+      echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+      die();
+    }
+  }
+
+  function insertNewCustomer($firstName, $lastName, $soSecNum) {
+    global $mysqli;
+
+    // Prepare the insert statment
+    if (!($stmt = $mysqli->prepare("INSERT INTO customer(first_name, last_name, social_security_num) VALUES (?, ?, ?)"))) {
+      echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+      die();
+    }
+
+    // Add values to SQL insert statement
+    if (!$stmt->bind_param("ssi", $firstName, $lastName, $soSecNum)) {
+      echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
+      die();
+    }
+
+    // Execute sql statement
+    if (!$stmt->execute()) {
+      echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+      die();
+    }
+  }
+
+  function getCustomerDbKey($ssNum) {
+    global $mysqli;
+
+    // Prepare the select statment
+    if (!($stmt = $mysqli->prepare("SELECT id FROM customer WHERE social_security_num=?;"))) {
+      echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+      die();
+    }
+
+    if (!$stmt->bind_param("i", $ssNum)) {
+          echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
+    }
+
+    if (!$stmt->execute()) {
+      echo "Execute failed: (" . $mysqli->errno . ") " . $mysqli->error;
+    }
+
+    if (!($res = $stmt->get_result())) {
+      echo "Getting result set failed: (" . $stmt->errno . ") " . $stmt->error;
+    }
+
+    // No results were returned, exit.
+    if ($res->num_rows === 0) {
+      return;
+    }
+
+    $res->data_seek(0);
+    $row = $res->fetch_assoc();
+
+    return $row['id'];
+  }
+
+  function getSingleCustomerInfo($ssNum) {
     global $mysqli;
 
     // Prepare the select statment
@@ -29,7 +277,6 @@
       die();
     }
 
-    $ssNum = $_GET['ssNum'];
     if (!$stmt->bind_param("i", $ssNum)) {
           echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
     }
@@ -85,5 +332,70 @@
 
     $finaljson = json_encode($customerArr);
     echo $finaljson;
+  }
+
+  function getAllStockSymbols() {
+    global $mysqli;
+
+    // Prepare the select statment
+    if (!($stmt = $mysqli->prepare("SELECT stock_symbol FROM stocks;"))) {
+      echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+      die();
+    }
+
+    if (!$stmt->execute()) {
+      echo "Execute failed: (" . $mysqli->errno . ") " . $mysqli->error;
+    }
+
+    if (!($res = $stmt->get_result())) {
+      echo "Getting result set failed: (" . $stmt->errno . ") " . $stmt->error;
+    }
+
+    // No results were returned, exit.
+    if ($res->num_rows === 0) {
+      return;
+    }
+
+    for($row_no = ($res->num_rows - 1); $row_no >= 0; $row_no--) {
+      $res->data_seek($row_no);
+      $row = $res->fetch_assoc();
+
+      $stocksArr[] = json_encode($row);
+    }
+
+    $finaljson = json_encode($stocksArr);
+    echo $finaljson;
+  }
+
+  function getStockDbKey($stkSym) {
+    global $mysqli;
+
+    // Prepare the select statment
+    if (!($stmt = $mysqli->prepare("SELECT id FROM stocks WHERE stock_symbol=?;"))) {
+      echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+      die();
+    }
+
+    if (!$stmt->bind_param("s", $stkSym)) {
+          echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
+    }
+
+    if (!$stmt->execute()) {
+      echo "Execute failed: (" . $mysqli->errno . ") " . $mysqli->error;
+    }
+
+    if (!($res = $stmt->get_result())) {
+      echo "Getting result set failed: (" . $stmt->errno . ") " . $stmt->error;
+    }
+
+    // No results were returned, exit.
+    if ($res->num_rows === 0) {
+      return;
+    }
+
+    $res->data_seek(0);
+    $row = $res->fetch_assoc();
+
+    return $row['id'];
   }
 ?>
